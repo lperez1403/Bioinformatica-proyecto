bf5d831245774796f456bedd2005bef504f51894
Laura Pérez López <lperez21@ucm.es>
2026-03-25 21:33:37 +0100

    Integrar traceback para recuperar los pares óptimos

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from nussinov import nussinov
+from traceback import traceback
 
 if __name__ == "__main__":
     secuencia = "GGGAAAUCC"
     matriz = nussinov(secuencia)
 
+    pares = traceback(matriz, secuencia, 0, len(secuencia) - 1, [])
+
     print("Secuencia:", secuencia)
     print("Número máximo de pares:", matriz[0][len(secuencia) - 1])
+    print("Pares encontrados:", pares)
     print("\nMatriz:")
 
     for fila in matriz:
