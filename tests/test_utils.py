a9e24462a2ebde06d836bbfa321b13b99556a682
Laura Pérez López <lperez21@ucm.es>
2026-03-25 21:39:20 +0100

    Añadir entorno virtual y dependencias del proyecto

---
+++ tests/test_utils.py
@@ -0,0 +1,12 @@
+from src.utils import es_par_valido, pares_a_dot_bracket
+
+def test_es_par_valido():
+    assert es_par_valido("A", "U")
+    assert es_par_valido("G", "C")
+    assert not es_par_valido("A", "C")
+
+
+def test_pares_a_dot_bracket():
+    pares = [(0, 5), (1, 4)]
+    estructura = pares_a_dot_bracket(6, pares)
+    assert estructura == "((..))"
