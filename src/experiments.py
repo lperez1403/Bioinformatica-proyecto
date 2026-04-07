91eeff3b006fba2e9bc7d93a7bf70758087cc4de
gloriadi <gloridia@ucm.es>
2026-04-07 18:44:11 +0200

    Avance codigo

--- src/experiments.py
+++ src/experiments.py
@@ -1,5 +1,5 @@
-from nussinov import nussinov
-from scoring import evaluar_algoritmo
+#*from nussinov import nussinov
+#from scoring import evaluar_algoritmo
 
 secuencias = [
     "GGGAAAUCC",
@@ -10,3 +10,21 @@ secuencias = [
 for seq in secuencias:
     resultado = evaluar_algoritmo(nussinov, seq)
     print(resultado)
+    
+  from fasta_parser import leer_fasta
+from nussinov import nussinov
+from scoring import evaluar_algoritmo
+
+def ejecutar_experimento(ruta_fasta):
+    secuencia = leer_fasta(ruta_fasta)
+    resultado = evaluar_algoritmo(nussinov, secuencia)
+
+    print("===================================")
+    print(f"Archivo: {ruta_fasta}")
+    print(f"Longitud: {resultado['longitud']}")
+    print(f"Pares: {resultado['pares']}")
+    print(f"Tiempo: {resultado['tiempo']:.5f} s")
+    print("===================================\n")
+
+if __name__ == "__main__":
+    ejecutar_experimento("data/raw/ejemplo.fasta")
